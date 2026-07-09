35eaa735bfe25a217ba956c78728851065334bec
Kishna Solanki <solanki.kishna20@gmail.com>
2026-07-09 22:29:17 +0530

    developed FastAPI backend talk to Ollama

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
-ECHO is on.
+from fastapi import FastAPI
+from pydantic import BaseModel
+import ollama
+
+app1 = FastAPI()
+
+class ChatRequest(BaseModel):
+    message : str
+
+@app1.post("/chat")
+def chat(request: ChatRequest):
+    response = ollama.chat(
+        model = "llama3.2",
+        messages = [
+            {
+                "role": "user",
+                "content": request.message,
+            }
+        ]
+    )
+
+    return{
+        "reply":response["message"]["content"]
+    }
